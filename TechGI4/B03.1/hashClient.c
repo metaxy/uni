72770cb5cfc55e5d11c11c50035376be0ca24862
Paul Walger <metaxy@walger.name>
2014-05-17 10:37:14 +0200

    hash communication

--- TechGI4/B03.1/hashClient.c
+++ TechGI4/B03.1/hashClient.c
@@ -19,36 +19,39 @@
 #include <netdb.h>
 
 #define MAX_BUFFER_LENGTH 100
-//int sockfd;
-//struct sockaddr_in their_addr; // connector's address information
+int sockfd;
+struct sockaddr_in their_addr; // connector's address information
 
-void unpackData(unsigned char *buffer, char command[], unsigned int *a, unsigned int *b)
+void unpackData(unsigned char *buffer, char *command, unsigned int *a, unsigned int *b)
 {
-    printf("%s", buffer);
-    *command = *buffer;
-    *a = (buffer[4]<<8) | buffer[5];
-    *b = (buffer[6]<<8) | buffer[7];
+    printf("unpack %s\n", buffer);
+    command[0] = buffer[0];
+    command[1] = buffer[1];
+    command[2] = buffer[2];
+    command[3] = buffer[3];
+    if(a != NULL) {
+        *a = (buffer[4]<<8) | buffer[5];
+    }
+    if(b != NULL) {
+        *b = (buffer[6]<<8) | buffer[7];
+    }
 }
 
 int packData(unsigned char *buffer, char command[], unsigned int a, unsigned int b) 
 {
-    //*buffer = command;
-  /*  buffer[4] = htons(a) & 0xFF;
+    buffer[0] = command[0];
+    buffer[1] = command[1];
+    buffer[2] = command[2];
+    buffer[3] = command[3];
+    buffer[4] = htons(a) & 0xFF;
     buffer[5] = htons(a) >> 8;
     
     buffer[6] = htons(b) & 0xFF;
-    buffer[7] = htons(b) >> 8;*/
-    
-    buffer[0] = 'A';
-    buffer[1] = 'B';
-    buffer[2] = '\n';
-    printf("buffer in pack = %s\n", buffer);
+    buffer[7] = htons(b) >> 8;
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd;
-struct sockaddr_in their_addr; // connector's address information
     struct hostent *he;
     int numbytes;
     int serverPort;
@@ -80,17 +83,12 @@ struct sockaddr_in their_addr; // connector's address information
     their_addr.sin_port = htons(serverPort);
     their_addr.sin_addr = *((struct in_addr *)he->h_addr);
     memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
-    unsigned char buffer[8];
-    packData(buffer, "asd", 1, 2);
-    
-    sendto(sockfd, buffer, sizeof(char)*4, 0, (struct sockaddr *)&their_addr, sizeof(struct sockaddr_in));
-    
-    recvfrom(sockfd, buffer, 4, 0, NULL, NULL);
+    set(12,12);
     close(sockfd);
 
     return 0;
 }
-/*
+
 int send_data(char command[], int key, int val)
 {
     printf("send_data %s %i %i\n", command, key, val);
@@ -99,7 +97,6 @@ int send_data(char command[], int key, int val)
     printf("buffer = %s\n", buffer);
     
     if(sendto(sockfd, buffer, sizeof(char)*8, 0, (struct sockaddr *)&their_addr, sizeof(struct sockaddr_in)) < 0) {
-    
         printf("could not send\n");
     }
     perror ("The following error occurred");
@@ -108,23 +105,44 @@ int send_data(char command[], int key, int val)
 int receive(char *command, int *key, int *val)
 {
     unsigned char buffer[8];
-    recvfrom(sockfd, buffer, 8, 0, NULL, NULL);
+    recvfrom(sockfd, buffer, sizeof(char)*8, 0, NULL, NULL);
     unpackData(buffer, command, key, val);
 }
+
 int set(int key, int val)
 {
     send_data("SET", key, val);
     char ret[4];
+    int a,b;
     receive(ret, NULL, NULL);
+    if(ret[0] == 'O') {
+        return 0;
+    }
+    return 1;
 }
 
 int get(int key)
 {
     send_data("GET", key, 0);
+    int val;
+    char ret[4];
+    receive(ret, NULL, &val);
+    if(ret[0] == 'V') {
+        return val;
+    } else {
+        printf("key not found\n");
+        return 0;
+    }   
 }
 
 int del(int key)
 {
     send_data("DEL", key, 0);
-}*/
+    char ret[4];
+    receive(ret, NULL, NULL);
+    if(ret[0] == 'O') {
+        return 0;
+    }
+    return 1;
+}
 
--- TechGI4/B03.1/hashServer.c
+++ TechGI4/B03.1/hashServer.c
@@ -19,17 +19,44 @@
 #include <netdb.h>
 #define MAX_BUFFER_LENGTH 100
 
-void unpackData(unsigned char *buffer, char command[], unsigned int *a, unsigned int *b)
+struct entry {
+    int key;
+    int val;
+    int valid;
+};
+struct entry table[256];
+
+struct entry* get(int key)
 {
-    printf("%s", buffer);
-    *command = *buffer;
+    return NULL;
+}
+
+void set(int key, int val)
+{
+    
+}
+
+void del(int key)
+{
+    
+}
+void unpackData(unsigned char *buffer, char *command, unsigned int *a, unsigned int *b)
+{
+    printf("unpack %s\n", buffer);
+    command[0] = buffer[0];
+    command[1] = buffer[1];
+    command[2] = buffer[2];
+    command[3] = buffer[3];
     *a = (buffer[4]<<8) | buffer[5];
     *b = (buffer[6]<<8) | buffer[7];
 }
 
-int packData(unsigned char *buffer, char *command, unsigned int a, unsigned int b) 
+int packData(unsigned char *buffer, char command[], unsigned int a, unsigned int b) 
 {
-    *buffer = *command;
+    buffer[0] = command[0];
+    buffer[1] = command[1];
+    buffer[2] = command[2];
+    buffer[3] = command[3];
     buffer[4] = htons(a) & 0xFF;
     buffer[5] = htons(a) >> 8;
     
@@ -65,25 +92,40 @@ int main(int argc, char *argv[])
         printf("could not bind\n");
     }
 
-    listen(sockfd, 10);
+    listen(sockfd, 1);
     
     while(1) {
 		clilen = sizeof cli_addr;
         unsigned char buffer[8];
 
         int size = recvfrom(sockfd, buffer, sizeof(char)*8, 0,(struct sockaddr *) &cli_addr, &clilen);
-        perror ("The following error occurred");
 		if(size > 0) {
-            printf("received len  %i", size);
+            printf("received len  %i\n", size);
             unsigned int key,value;
             char command[4];
             unpackData(buffer, command, &key, &value);
-            
-            /*
-			sendto(sockfd, buffer, packSize,0, (struct sockaddr *) &cli_addr, clilen);*/
+    
+            if(command[0] == 'S') {
+                set(key, value);
+                packData(buffer, "OK!", 0, 0);
+            } else if(command[0] == 'G') {
+                struct entry *ret = get(key);
+                if(ret == 0) {
+                    packData(buffer, "NOF", 0, 0);
+                } else {
+                    packData(buffer, "VAL", ret->key, ret->val);
+                }
+            } else if(command[0] == 'D') {
+                del(key);
+                packData(buffer, "OK!", 0, 0);
+            }
+			sendto(sockfd, buffer, 8, 0, (struct sockaddr *) &cli_addr, clilen);
         }
     }
     close(sockfd);
     return 0;
 }
+
+
+
 
