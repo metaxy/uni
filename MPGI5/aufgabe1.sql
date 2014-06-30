7c3e1c5357dbaac1e3138a0ea038ee9ded3b461d
Paul Walger <metaxy@walger.name>
2014-06-30 16:10:00 +0200

    Update aufgabe1.sql

--- MPGI5/aufgabe1.sql
+++ MPGI5/aufgabe1.sql
@@ -5,15 +5,6 @@ CREATE TABLE IF NOT EXISTS `Abteilung` (
   PRIMARY KEY (`AbteilungsNr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Abteilung` (`AbteilungsNr`, `Name`, `Ort`) VALUES
-(1, 'Test', 'Berlin'),
-(2, 'Entwicklung', 'Berlin'),
-(3, 'Anforderungsanagement', 'Köln'),
-(4, 'Marketing', 'München'),
-(5, 'Analyse', 'Hamburg'),
-(6, 'Controlling', 'Berlin'),
-(7, 'Beratung', 'Hamburg');
-
 CREATE TABLE IF NOT EXISTS `Angestellter` (
   `PersonalNr` int(11) NOT NULL,
   `Name` text NOT NULL UNIQUE,
@@ -25,32 +16,12 @@ CREATE TABLE IF NOT EXISTS `Angestellter` (
   KEY `AbteilungsNr` (`AbteilungsNr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Angestellter` (`PersonalNr`, `Name`, `Vorname`, `Gehalt`, `Wohnort`, `AbteilungsNr`) VALUES
-(1, 'Winter', 'Carolin', 45000, 'Berlin', 2),
-(2, 'Sommer', 'Anna', 35000, 'Hamburg', 4),
-(3, 'Frühling', 'Leonie', 60000, 'Berlin', 3),
-(4, 'Kühlsommer', 'Peter', 45000, 'München', 2),
-(5, 'Spätfrühling', 'Max', 55000, 'Köln', 6),
-(6, 'Spätsommer', 'Marion', 56000, 'München', 1),
-(7, 'Frischherbst', 'Frank', 42000, 'Stuttgart', 7),
-(8, 'Warmwinter', 'Paul', 43000, 'Potsdam', 5);
-
 CREATE TABLE IF NOT EXISTS `ArbeitetMit` (
   `PersonalNr` int(11) NOT NULL,
   `ProjektNr` int(11) NOT NULL,
   PRIMARY KEY (`PersonalNr`,`ProjektNr`),
   KEY `ArbeitetMit_ibfk_2` (`ProjektNr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `ArbeitetMit` (`PersonalNr`, `ProjektNr`) VALUES
-(8, 3),
-(7, 5),
-(4, 21),
-(6, 21),
-(2, 29),
-(1, 31),
-(5, 31),
-(8, 31);
 
 CREATE TABLE IF NOT EXISTS `Projekt` (
   `ProjektNr` int(11) NOT NULL,
@@ -63,14 +34,6 @@ CREATE TABLE IF NOT EXISTS `Projekt` (
   KEY `Leiter` (`Leiter`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Projekt` (`ProjektNr`, `ProjektName`, `ProjektOrt`, `istTeilprojektVon`, `Leiter`) VALUES
-(3, 'Implementierung', 'Köln', 23, 4),
-(5, 'Logischer Entwurf', 'München', 3, 2),
-(21, 'Benchmark', 'Stuttgart', 23, 6),
-(23, 'VOIP', 'Hamburg', NULL, 3),
-(25, 'ER-Entwurf', 'Berlin', 23, 2),
-(29, 'Anforderungsanalyse', 'Berlin', 25, 7),
-(31, 'Anforderungserhebung', 'Berlin', 25, 3);
 
 ALTER TABLE `Angestellter`
   ADD CONSTRAINT `Angestellter_ibfk_1` FOREIGN KEY (`AbteilungsNr`) REFERENCES `Abteilung` (`AbteilungsNr`) ON DELETE NO ACTION ON UPDATE CASCADE;
@@ -82,3 +45,42 @@ ALTER TABLE `ArbeitetMit`
 ALTER TABLE `Projekt`
   ADD CONSTRAINT `Projekt_ibfk_1` FOREIGN KEY (`istTeilprojektVon`) REFERENCES `Projekt` (`ProjektNr`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `Projekt_ibfk_2` FOREIGN KEY (`Leiter`) REFERENCES `Angestellter` (`PersonalNr`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+INSERT INTO `ArbeitetMit` (`PersonalNr`, `ProjektNr`) VALUES
+(8, 3),
+(7, 5),
+(4, 21),
+(6, 21),
+(2, 29),
+(1, 31),
+(5, 31),
+(8, 31);
+
+INSERT INTO `Projekt` (`ProjektNr`, `ProjektName`, `ProjektOrt`, `istTeilprojektVon`, `Leiter`) VALUES
+(3, 'Implementierung', 'Köln', 23, 4),
+(5, 'Logischer Entwurf', 'München', 3, 2),
+(21, 'Benchmark', 'Stuttgart', 23, 6),
+(23, 'VOIP', 'Hamburg', NULL, 3),
+(25, 'ER-Entwurf', 'Berlin', 23, 2),
+(29, 'Anforderungsanalyse', 'Berlin', 25, 7),
+(31, 'Anforderungserhebung', 'Berlin', 25, 3);
+
+INSERT INTO `Angestellter` (`PersonalNr`, `Name`, `Vorname`, `Gehalt`, `Wohnort`, `AbteilungsNr`) VALUES
+(1, 'Winter', 'Carolin', 45000, 'Berlin', 2),
+(2, 'Sommer', 'Anna', 35000, 'Hamburg', 4),
+(3, 'Frühling', 'Leonie', 60000, 'Berlin', 3),
+(4, 'Kühlsommer', 'Peter', 45000, 'München', 2),
+(5, 'Spätfrühling', 'Max', 55000, 'Köln', 6),
+(6, 'Spätsommer', 'Marion', 56000, 'München', 1),
+(7, 'Frischherbst', 'Frank', 42000, 'Stuttgart', 7),
+(8, 'Warmwinter', 'Paul', 43000, 'Potsdam', 5);
+
+
+INSERT INTO `Abteilung` (`AbteilungsNr`, `Name`, `Ort`) VALUES
+(1, 'Test', 'Berlin'),
+(2, 'Entwicklung', 'Berlin'),
+(3, 'Anforderungsanagement', 'Köln'),
+(4, 'Marketing', 'München'),
+(5, 'Analyse', 'Hamburg'),
+(6, 'Controlling', 'Berlin'),
+(7, 'Beratung', 'Hamburg');
